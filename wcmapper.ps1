043085b045706a7cc48ecf97aa884dc9c7a0f7e3
Monty1782 <monty.python1782@gmail.com>
2020-06-14 23:05:42 +0200

    Generated new map

---
+++ wcmapper.ps1
@@ -0,0 +1,36 @@
+﻿
+### creates the map
+
+$folders_to_map = "Liveries", "KneeBoard" # <= add relative path of folders to map (ex: "Mods\tech")
+
+$base_path = pwd
+
+
+[IO.FileInfo] $map_file = $base_path.Path + '\wcmap.txt'
+if ($map_file.Exists){
+    rm $map_file
+}
+
+function Map-Directory{
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$path
+    )
+
+    Get-ChildItem -Recurse $path | ForEach-Object {
+    $gethash = Get-FileHash $_.FullName
+    $hash = $gethash.Hash
+    $fullName = $_.FullName.Substring($base_path.Path.Length)
+    Write-Output "$hash, '$fullName'" >> wcmap.txt
+    Write-Output $_.FullName
+}
+
+}
+
+
+$folders_to_map | ForEach-Object {
+    $folder = $base_path.Path +'\'+ $_
+    Write-Output $folder
+    Map-Directory $folder
+}
