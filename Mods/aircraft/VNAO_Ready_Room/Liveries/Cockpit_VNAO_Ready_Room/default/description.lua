20f5767619c0dd149cc3f4ddd538aaadf86f0b40
Monty1782 <monty.python1782@gmail.com>
2020-06-16 22:49:38 +0200

    CRLF

--- Mods/aircraft/VNAO_Ready_Room/Liveries/Cockpit_VNAO_Ready_Room/default/description.lua
+++
@@ -1,3 +0,0 @@
-livery = {
-	{"mirror", 0 ,"mirrors",true};--mirrors texture replacement with runtime rendered one
-}
--- Mods/aircraft/VNAO_Ready_Room/Liveries/VNAO_Ready_Room/default/description.lua
+++
@@ -1,5 +0,0 @@
-livery = {
-	
-}
-name = "default"
-countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
