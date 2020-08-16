45aaefd2c12e71ffe4d553be4c713618bd5a3207
drumbart <bartek.tulodziecki@gmail.com>
2020-08-16 17:38:24 +0200

    added iriaf f-5 livery

---
+++ Liveries/f-5e-3/IRIAF F-5E 1980s pack1/IRIAF 3-7059 1980s/description.lua
@@ -0,0 +1,52 @@
+livery = {
+        {"F-5E3_bort_number_nose_100", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_bort_number_nose_100", DECAL ,"empty",true};
+	
+        {"F-5E3_bort_number_nose_10", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_bort_number_nose_10", DECAL ,"empty",true};
+        
+        {"F-5E3_bort_number_nose_1", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_bort_number_nose_1", DECAL ,"empty",true};
+	
+        {"F-5E3_bort_number_nose_10A", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_bort_number_nose_10A", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_nose_1A", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_bort_number_nose_1A", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_rude_100", 0 ,"f-5e3_rude_37059",false};
+	{"F-5E3_bort_number_rude_100", DECAL ,"empty",true};
+	
+	{"F-5E3_bort_number_rude_10", 0 ,"f-5e3_rude_37059",false};
+	{"F-5E3_bort_number_rude_10", DECAL ,"empty",true};
+
+        {"F-5E3_bort_number_rude_1", 0 ,"f-5e3_rude_37059",false};
+         {"F-5E3_bort_number_rude_1", DECAL ,"empty",true};
+
+	{"F-5E3_bort_number_rude_10A", 0 ,"f-5e3_rude_37059",false};
+	{"F-5E3_bort_number_rude_10A", DECAL ,"empty",true};	
+	
+	{"F-5E3_bort_number_rude_1A", 0 ,"f-5e3_rude_37059",false};
+	{"F-5E3_bort_number_rude_1A", DECAL ,"empty",true};
+	
+        {"Material #0", 0, "ptb_275_iriaf2", false};
+        {"PTB_150", 0, "ptb_150_iriaf", false};	
+
+	{"F-5E3_nose", 0 ,"f-5e3_nose_37059",false};
+	{"F-5E3_tail", 0 ,"f-5e3_tail_37059",false};
+	{"F-5E3_rude", 0 ,"f-5e3_rude_37059",false};
+	{"F-5E3_wing", 0 ,"f-5e3_wing_37059",false};
+	
+	{"pilot_F5_E3", 0 ,"f-5e3_iriaf_pilot1",false};
+	{"pilot_F5E3_helmet", 0 ,"f-5e3_iriaf_helmet",false};
+	{"pilot_F15_patch", 0 ,"f-5e3_iriaf_PilotPatch1",false};
+}
+
+name = "IRIAF 3-7059 1980s"
+
+countries = {"IRN"}
+
+
+-- made by Sadjad_Vosoul
+
+
