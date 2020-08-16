e9c2e106a36df87e4c938600c7a2463118b24a23
drumbart <bartek.tulodziecki@gmail.com>
2020-08-16 20:23:20 +0200

    adde mirage livery

---
+++ Liveries/m-2000c/IRIAF Blue-Gray/description.lua
@@ -0,0 +1,71 @@
+livery = {
+	{"ext1", 0 ,"M2KC_1T",false};
+	{"ext2", 0 ,"M2KC_2T",false};
+	
+        {"pilot_M2KC_patch", 0 ,"Pilot_M2KC_patch_IRN",false};
+
+	{"FUSE_NUMBER1_001", 0 ,"M2KC_1T",false};
+	{"FUSE_NUMBER1_001", DECAL ,"empty",true};
+
+	{"FUSE_NUMBER2_001", 0 ,"M2KC_1T",false};
+	{"FUSE_NUMBER2_001", DECAL ,"empty",true};
+
+	{"FUSE_NUMBER2_010", 0 ,"M2KC_1T",false};
+	{"FUSE_NUMBER2_010", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER1_001", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER1_001", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER1_010", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER1_010", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER1_100", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER1_100", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER2_001", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER2_001", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER2_010", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER2_010", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER2_100", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER2_100", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER3_001", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER3_001", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER3_010", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER3_010", DECAL ,"empty",true};
+
+	{"TAIL_NUMBER3_100", 0 ,"M2KC_2T",false};
+	{"TAIL_NUMBER3_100", DECAL ,"empty",true};
+
+	{"NOSE_NUMBER_001", 0 ,"M2KC_1T",false};
+	{"NOSE_NUMBER_001", DECAL ,"empty",true};
+
+	{"NOSE_NUMBER_010", 0 ,"M2KC_1T",false};
+	{"NOSE_NUMBER_010", DECAL ,"empty",true};
+
+	{"NOSE_NUMBER_100", 0 ,"M2KC_1T",false};
+	{"NOSE_NUMBER_100", DECAL ,"empty",true};
+
+	{"WINGR_NUMBER_001", 0 ,"M2KC_2T",false};
+	{"WINGR_NUMBER_001", DECAL ,"empty",true};
+
+	{"WINGR_NUMBER_010", 0 ,"M2KC_2T",false};
+	{"WINGR_NUMBER_010", DECAL ,"empty",true};
+
+	{"WINGR_NUMBER_100", 0 ,"M2KC_2T",false};
+	{"WINGR_NUMBER_100", DECAL ,"empty",true};
+
+	{"WINGL_NUMBER_001", 0 ,"M2KC_1T",false};
+	{"WINGL_NUMBER_001", DECAL ,"empty",true};
+
+	{"WINGL_NUMBER_010", 0 ,"M2KC_1T",false};
+	{"WINGL_NUMBER_010", DECAL ,"empty",true};
+
+	{"WINGL_NUMBER_100", 0 ,"M2KC_1T",false};
+	{"WINGL_NUMBER_100", DECAL ,"empty",true};
+}
+name = "IRIAF Blue-Gray"
+countries = {"IRN", "FRA"}
