a4b12265337fcea5c8931c5310fd2122515f8731
drumbart <bartek.tulodziecki@gmail.com>
2021-03-30 17:57:55 +0200

    added new 14th liveries

---
+++ Liveries/F-16C_50/14th FS MrEdd/description.lua
@@ -0,0 +1,134 @@
+livery = {
+
+	{"F16_bl50_Kil", 0 ,"F16_bl50_Kil",false};
+	{"F16_bl50_Kil", 13 ,"F16_bl50_Kil_RoughMet",false};
+	{"F16_bl50_Kil", 1,"F16_bl50_Kil_Normal",true};
+  
+	{"F16_bl50_Main_1", 0 ,"F16_bl50_Main_1",false};
+	{"F16_bl50_Main_1", ROUGHNESS_METALLIC, "f16_bl50_main_1_roughmet", false};
+	{"F16_bl50_Main_1", 1,"F16_bl50_Main_1_Normal",true};
+	
+	{"F16_bl50_Main_2", 0 ,"F16_bl50_Main_2",false};
+	{"F16_bl50_Main_2", 13 ,"F16_bl50_Main_2_RoughMet",false};
+
+    {"F16_bl50_Main_3", 0 ,"F16_bl50_Main_3",false};
+	{"F16_bl50_Main_3", 13 ,"F16_bl50_Main_3_RoughMet",false};
+
+	{"F16_bl50_Wing_Pylon_1", 0 ,"F16_bl50_Wing_Pylon_1",false};
+	{"F16_bl50_Wing_Pylon_2", 0 ,"F16_bl50_Wing_Pylon_2",false};
+	
+	{"F16_bl50_wing_L", 0 ,"F16_bl50_wing_L",false};
+	{"F16_bl50_wing_L", 13 ,"F16_bl50_wing_L_RoughMet",false};
+	{"F16_bl50_wing_L", 1,"F16_bl50_wing_L_Normal",true};
+
+	{"F16_bl50_wing_R", 0 ,"F16_bl50_wing_R",false};
+	{"F16_bl50_wing_R", 13 ,"F16_bl50_wing_R_RoughMet",false};		
+	{"F16_bl50_wing_R", 1,"F16_bl50_wing_R_Normal",true};
+	
+	{"F16_bl50_Gear", 0, "f16_bl50_gear", true};
+	{"F16_bl50_Tyres",0,"F16_bl50_Tyres",true};
+	{"F16_bl50_Tyres", 13 ,"F16_bl50_Tyres_RoughMet",true};
+
+	{"LAU_129", 0 ,"LAU_129_diff",false};
+	{"LAU_129",ROUGHNESS_METALLIC,"LAU_129_diff_RoughMet",true};
+	{"F16_bl50_Wing_Pylon_1", 0 ,"F16_bl50_Wing_Pylon_1",false};
+	{"F16_bl50_Wing_Pylon_2", 0 ,"F16_bl50_Wing_Pylon_2",false};	
+	
+	
+	{"Tank_370", DIFFUSE, "fuel_tank_370gal_diff", false};
+	{"PTB_300Gal", 0, "fuel_tank_300gal", false};
+	
+		--F16_bl50_NOUSE_1_DECAL
+    {"F16_bl50_NOUSE_1_DECAL",    0,                    "f16_bl50_main_1",            false};
+    {"F16_bl50_NOUSE_1_DECAL",    1,                    "f16_bl50_main_1_normal",    true};
+    {"F16_bl50_NOUSE_1_DECAL",    ROUGHNESS_METALLIC,    "f16_bl50_main_1_roughmet", false};
+    {"F16_bl50_NOUSE_1_DECAL",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_1_BORT_NUMBER_001
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_001",    0,                    "f16_bl50_main_1",            false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_001",    1,                    "f16_bl50_main_1_normal",    true};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_001",    ROUGHNESS_METALLIC,    "f16_bl50_main_1_roughmet", false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_001",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_1_BORT_NUMBER_010
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_010",    0,                    "f16_bl50_main_1",            false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_010",    1,                    "f16_bl50_main_1_normal",    true};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_010",    ROUGHNESS_METALLIC,    "f16_bl50_main_1_roughmet", false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_010",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_1_BORT_NUMBER_100
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_100",    0,                    "f16_bl50_main_1",            false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_100",    1,                    "f16_bl50_main_1_normal",    true};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_100",    ROUGHNESS_METALLIC,    "f16_bl50_main_1_roughmet", false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_100",    DECAL,                "empty", true};
+ --F16_bl50_NOUSE_1_BORT_NUMBER_X001	  
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_X100", DIFFUSE, "f16_bl50_main_1", false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_X100", NORMAL_MAP, "f16_bl50_main_1_normal", true};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_X100", ROUGHNESS_METALLIC, "f16_bl50_main_1_roughmet", false};
+    {"F16_bl50_NOUSE_1_BORT_NUMBER_X100", DECAL, "empty", true};
+ 
+--F16_bl50_NOUSE_2_BORT_NUMBER_001
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_001",    0,                    "f16_bl50_main_2",            false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_001",    1,                    "f16_bl50_main_2_normal",    true};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_001",    ROUGHNESS_METALLIC,    "f16_bl50_main_2_roughmet", false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_001",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_2_BORT_NUMBER_010
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_010",    0,                    "f16_bl50_main_2",            false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_010",    1,                    "f16_bl50_main_2_normal",    true};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_010",    ROUGHNESS_METALLIC,    "f16_bl50_main_2_roughmet", false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_010",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_2_BORT_NUMBER_100
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_100",    0,                    "f16_bl50_main_2",            false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_100",    1,                    "f16_bl50_main_2_normal",    true};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_100",    ROUGHNESS_METALLIC,    "f16_bl50_main_2_roughmet", false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_100",    DECAL,                "empty", true};
+--F16_bl50_NOUSE_2_BORT_NUMBER_X001	
+	{"F16_bl50_NOUSE_2_BORT_NUMBER_X001", DIFFUSE, "f16_bl50_main_2", false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_X001", NORMAL_MAP, "f16_bl50_main_2_normal", true};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_X001", ROUGHNESS_METALLIC, "f16_bl50_main_2_roughmet", false};
+    {"F16_bl50_NOUSE_2_BORT_NUMBER_X001", DECAL, "empty", true};
+    
+--F16_bl50_FIN_DECAL    
+    {"F16_bl50_FIN_DECAL",    0,                    "f16_bl50_kil",                false};
+    {"F16_bl50_FIN_DECAL",    1,                    "f16_bl50_kil_normal",        true};
+    {"F16_bl50_FIN_DECAL",    ROUGHNESS_METALLIC,    "f16_bl50_kil_roughmet",    false};
+    {"F16_bl50_FIN_DECAL",    DECAL,                "F16_AF_91_Decal",    false};
+
+--F16_bl50_FIN_BORT_NUMBER_001
+    {"F16_bl50_FIN_BORT_NUMBER_001",    0,                    "f16_bl50_kil",                false};
+    {"F16_bl50_FIN_BORT_NUMBER_001",    1,                    "f16_bl50_kil_normal",        true};
+    {"F16_bl50_FIN_BORT_NUMBER_001",    ROUGHNESS_METALLIC,    "f16_bl50_kil_roughmet",    false};
+    {"F16_bl50_FIN_BORT_NUMBER_001",    DECAL,                      "F16_bort_number", false};
+
+--F16_bl50_FIN_BORT_NUMBER_010
+    {"F16_bl50_FIN_BORT_NUMBER_010",    0,                    "f16_bl50_kil",                false};
+    {"F16_bl50_FIN_BORT_NUMBER_010",    1,                    "f16_bl50_kil_normal",        true};
+    {"F16_bl50_FIN_BORT_NUMBER_010",    ROUGHNESS_METALLIC,    "f16_bl50_kil_roughmet",    false};
+    {"F16_bl50_FIN_BORT_NUMBER_010",    DECAL,                      "F16_bort_number", false};
+    
+--F16_bl50_FIN_BORT_NUMBER_100
+    {"F16_bl50_FIN_BORT_NUMBER_100",    0,                    "f16_bl50_kil",                false};
+    {"F16_bl50_FIN_BORT_NUMBER_100",    1,                    "f16_bl50_kil_normal",        true};
+    {"F16_bl50_FIN_BORT_NUMBER_100",    ROUGHNESS_METALLIC,    "f16_bl50_kil_roughmet",    false};
+    {"F16_bl50_FIN_BORT_NUMBER_100",    DECAL,                       "F16_bort_number", false};
+	
+	
+	-- Pilot
+		
+	{"pilot_F16_helmet", 0,	                    "pilot_f16_helmet",				false};
+	{"pilot_F16_helmet", 1,	                    "pilot_f16_helmet_nm",			false};
+	{"pilot_F16_helmet", ROUGHNESS_METALLIC,	"pilot_f16_helmet_roughmet",	true};
+	
+		{"pilot_F16",	     0,	                    "pilot_f16_green",			    false};
+	{"pilot_F16",	     1,	                    "pilot_f16_nm",			        true};
+	{"pilot_F16",	     ROUGHNESS_METALLIC,	"pilot_f16_roughmet",	        true};
+	
+	{"pilot_F16_patch", 0, "pilot_f16_patch", false};
+	{"pilot_F16_patch", NORMAL_MAP, "pilot_f16_patch_nm", true};
+	{"pilot_F16_patch", ROUGHNESS_METALLIC, "pilot_f16_patch_roughmet", true};
+}
+name = "14th FS - MrEdd"
+custom_args = 
+{
+[1000] = 1.0, -- change of type of board number (0.0 -default USA, 0.1, 0.2, 0.3, 1.0 not drawn)
+[1001] = 1.0, -- vis refuel board number (0.0 drawn, 1.0 not drawn)
+[1002] = 1.0, -- change of type intake board number (0.0 two digits, 0.1 three digits, 1.0 not drawn)
+[1003] = 1.0, -- vis nouse board number (0.0 drawn, 1.0 not drawn)
+}
