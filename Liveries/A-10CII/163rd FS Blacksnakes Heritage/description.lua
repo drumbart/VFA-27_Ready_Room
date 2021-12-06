6770e9bdb3f3c4da68060566986852e74e52ac67
Ravasempai <88625562+Ravasempai@users.noreply.github.com>
2021-12-06 17:23:30 +0000

    A10 liveries

---
+++ Liveries/A-10CII/163rd FS Blacksnakes Heritage/description.lua
@@ -0,0 +1,36 @@
+livery = {
+
+	-- Roughmets
+	
+	{"A-10C_PAINT_1-a",	13			,	"A10CII_1a_RoughMet", false};
+	{"A-10C_PAINT_1-b",	13			,	"A10CII_1b_RoughMet", false};
+	{"A-10C_PAINT_1-c",	13			,	"A10CII_1c_RoughMet", false};
+	{"A-10C_PAINT_1-d",	13			,	"A10CII_1d_RoughMet", false};
+	{"A-10C_PAINT_1-e",	13			,	"A10CII_1e_RoughMet", false};
+	{"A-10C_PAINT_1-f",	13			,	"A10CII_1f_RoughMet", false};
+	{"A-10C_PAINT_1-g",	13			,	"A10CII_1g_RoughMet", false};
+	{"A-10C_PAINT_1-h",	13			,	"A10CII_1h_RoughMet", false};
+	{"A-10C_PAINT_1-i",	13			,	"A10CII_1i_RoughMet", false};
+	{"A-10C_PAINT_1-j",	13			,	"A10CII_1j_RoughMet", false};
+	{"A-10C_PAINT_1-k",	13			,	"A10CII_1k_RoughMet", false};
+	{"A-10C_PAINT_1-L",	13			,	"A10CII_1L_RoughMet", false};
+	
+	-- Main Diffuse
+
+	{"A-10C_PAINT_1-a", 0 ,"A-10C_104_1-a",false};
+	{"A-10C_PAINT_1-b", 0 ,"A-10C_104_1-b",false};
+	{"A-10C_PAINT_1-c", 0 ,"A-10C_104_1-c",false};
+	{"A-10C_PAINT_1-d", 0 ,"A-10C_104_1-d",false};
+	{"A-10C_PAINT_1-e", 0 ,"A-10C_104_1-e",false};
+	{"A-10C_PAINT_1-f", 0 ,"A-10C_104_1-f",false};
+	{"A-10C_PAINT_1-g", 0 ,"A-10C_104_1-g",false};
+	{"A-10C_PAINT_1-h", 0 ,"A-10C_104_1-h",false};
+	{"A-10C_PAINT_1-i", 0 ,"A-10C_104_1-i",false};
+	{"A-10C_PAINT_1-j", 0 ,"A-10C_104_1-j",false};
+	{"A-10C_PAINT_1-k", 0 ,"A-10C_104_1-k",false};
+	{"A-10C_PAINT_1-L", 0 ,"A-10C_104_1-l",false};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number_Wheel", 0 ,"TactNumbers-USAF-Light_black",true};
+}
+countries = {"USA"}
