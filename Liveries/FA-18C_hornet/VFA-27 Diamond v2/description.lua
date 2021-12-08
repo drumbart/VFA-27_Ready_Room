8223ad91bbab4f48e839c3c88ee3baa818935fc8
Ravasempai <88625562+Ravasempai@users.noreply.github.com>
2021-12-08 21:32:16 +0000

    Livery

--- Liveries/FA-18C_hornet/VFA-27 Diamond v2/description.lua
+++ Liveries/FA-18C_hornet/VFA-27 Diamond v2/description.lua
@@ -234,8 +234,8 @@ livery = {
 	{"F18C_BORT_NUMBER_MTW_aus_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
 	{"F18C_BORT_NUMBER_MTW_aus_L_01", DECAL ,"empty",true};
 
-	{"FPU_8A", 0 ,"FPU_8A_Diff",true};
-	{"FPU_8A", 2 ,"FPU_8A_Diff_RoughMet",true};
+	{"FPU_8A", 0 ,"FPU_8A_royal_maces_airborne_black_gold",false};
+	{"FPU_8A", ROUGHNESS_METALLIC ,"FPU_8A_Diff_RoughMet",true};
 
 
 
